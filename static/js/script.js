b5083fed72e589cfef94dd7cbd55e75f82499a46
thiagoluizfb <thiagoluizfb@gmail.com>
2020-11-14 23:08:48 +0000

    Added icons to purchase and register materials to inventory.html,  completed base.html style

--- static/js/script.js
+++ static/js/script.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
     $('.dropdown-trigger').dropdown();
     $('.tabs').tabs();
     $('.collapsible').collapsible();
-
+    $('.tooltipped').tooltip();
+    
     $('#purchase').click(function(){
         $('#purchases').css("visibility", "visible");
     })
