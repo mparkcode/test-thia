b749cb84448856acb9c127ff99100138b9294c19
thiagoluizfb <thiagoluizfb@gmail.com>
2020-11-15 18:30:25 +0000

    Completed actions for mobile nav panel with toggles fixed

--- static/js/script.js
+++ static/js/script.js
@@ -14,10 +14,32 @@ $(document).ready(function(){
 
     $('#plus-inventory').click(function(){
         $('#mobilepanel-plus-inventory').toggle();
+        $('#mobilepanel-caret').hide();
+        $('#mobilepanel-search-inventory').hide();
     })
 
-    $('.contentrow, .tabrow, nav, #search-inventory, #caret-inventory').click(function(){
+    $('#search-inventory').click(function(){
+        $('#mobilepanel-search-inventory').toggle();
+        $('#mobilepanel-caret').hide();
+        $('#mobilepanel-plus-inventory').hide();
+    })
+
+    $('.caret-up').click(function(){
+        $('#mobilepanel-caret').toggle();
+        $('#mobilepanel-plus-inventory').hide();
+        $('#mobilepanel-search-inventory').hide();
+    })
+
+    $('.contentrow, .tabrow, nav, #search-inventory, .caret-up ,#mobilepanel-plus-inventory').click(function(){
         $('#mobilepanel-plus-inventory').css("display", "none");
+    })
+
+    $('.contentrow, .tabrow, nav, #plus-inventory, .caret-up,#mobilepanel-plus-inventory').click(function(){
+        $('#mobilepanel-search-inventory').css("display", "none");
+    })
+
+     $('.contentrow, .tabrow, nav, #plus-inventory, #mobilepanel-search-inventory ,#mobilepanel-plus-inventory').click(function(){
+        $('#mobilepanel-caret').css("display", "none");
     })
 
   });
